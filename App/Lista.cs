85e3d9f3ef91bbf6d70fa3a3be1535b2130cf973
kleverson.cruz <kleverson.cruz@tecnospeed.com.br>
2022-07-27 09:37:05 -0300

    Add project files.

---
+++ App/Lista.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace ConverterXmlParaObjeto
+{
+    [XmlRoot("lista", Namespace = "", IsNullable = false)]
+    public partial class Lista
+    {
+        [XmlElement("musica")]
+        public listaMusica[] musicas { get; set; }
+    }
+
+    public partial class listaMusica
+    {
+        public string titulo { get; set; }
+
+        public string artista { get; set; }
+
+        public string duracao { get; set; }
+
+        [XmlArrayItem("genero", IsNullable = false)]
+        public string[] generos { get; set; }
+
+        [XmlAttribute()]
+        public byte id { get; set; }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConverterXmlParaObjeto;
+using System.Xml.Serialization;
+
+//Obtém o caminho do arquivo XML na pasta do projeto
+string diretorioProjeto = Directory.GetCurrentDirectory();
+string caminhoArquivo = Path.Combine(diretorioProjeto, "ListaDeMusicas.xml");
+
+Stream reader = new FileStream(caminhoArquivo, FileMode.Open);
+
+//Cria uma instância de XmlSerializer e desserializa para uma variavel do tipo Lista
+XmlSerializer serializer = new(typeof(Lista));
+Lista listaMusicas = (Lista)serializer.Deserialize(reader);
+
+//Inteira nos elementos da lista e gera mensagens no console com os dados.
+foreach (var musica in listaMusicas.musicas)
+{
+    Console.WriteLine($"#{musica.id} - {musica.titulo} - Duração: {musica.duracao}");
+    Console.WriteLine($"Artistas: {musica.artista}\n");
+}
